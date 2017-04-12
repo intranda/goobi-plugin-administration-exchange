28e036ff1ea12b3717a723077edd743d0f72a0a5
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2017-04-12 08:29:57 +0200

    removed unused shell files

--- goobi-plugin-administration-dump/mysql_export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/mysqldump -u goobi -pgoobi goobi > /opt/digiverso/goobi/tmp/goobi.sql
--- goobi-plugin-administration-dump/mysql_import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/mysql -u goobi -pgoobi goobi < /opt/digiverso/goobi/tmp/goobi.sql
-
